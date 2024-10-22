a46d77408af4f9b34f4696e85e62eac80fae8f1b
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2024-10-22 19:31:39 +0200

    Modification du script BD + Trigger poidsCavalier < poidsSupportablePoney

--- Analyse/sript.sql
+++ Analyse/sript.sql
@@ -1,10 +1,9 @@
 CREATE TABLE COURS (
-  PRIMARY KEY (coursID),
-  coursID INT(4) NOT NULL,
+  coursID INT(4) PRIMARY KEY AUTO_INCREMENT,
   typeC VARCHAR(42) CHECK (typeC IN ('Collectif', 'particulier')),
-  duree TIME,
-  nbParticipants INT(4),
-  jour VARCHAR(42),
+  duree INT(4) CHECK (duree IN (1, 2)),
+  nbParticipants INT(4) CHECK (nbParticipants <= 10),
+  jour VARCHAR(42) CHECK (jour IN ('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche')),
   heureD TIME,
   heureF TIME,
   prix DECIMAL(10,2),
@@ -12,29 +11,27 @@ CREATE TABLE COURS (
 );
 
 CREATE TABLE MEMBRE (
-  PRIMARY KEY (idM),
-  idM INT(4) NOT NULL,
+  idM INT(4) PRIMARY KEY AUTO_INCREMENT,
   nomM VARCHAR(42),
   prenomM VARCHAR(42),
   dateNaissance DATE,
-  email VARCHAR(42),
+  email VARCHAR(42) UNIQUE,
   telephone VARCHAR(42),
-  poidsA DECIMAL(10,2) NULL,
+  poidsA DECIMAL(10,2),
   niveau VARCHAR(42) NULL,
   idT INT(4) NULL,
-  cotisationAnnee INT(4) NULL,
-  payeeCotisation BOOLEAN NULL,
-  certificat VARCHAR(42) NULL
+  cotisationAnnee INT(4),
+  payeeCotisation BOOLEAN,
+  certificat VARCHAR(42)
 );
 
 CREATE TABLE PONEY (
-  PRIMARY KEY (poneyID),
-  poneyID INT(4) NOT NULL,
+  poneyID INT(4) PRIMARY KEY AUTO_INCREMENT,
   nomP VARCHAR(42),
   age INT(4),
-  poidsMaxCavalier DECIMAL(10,2),
-  dernierCours DATE,
-  tempsRepos INT(4)
+  poidsSupportableMax DECIMAL(10,2),
+  dernierCours DATE, -- DATETIME ???
+  disponible BOOLEAN
 );
 
 CREATE TABLE RESERVATION (
@@ -45,15 +42,16 @@ CREATE TABLE RESERVATION (
   coursPayee BOOLEAN
 );
 
-CREATE TABLE SEANCE (
-  dateSeance DATE,
-  seancePayee BOOLEAN,
-  coursID INT(4) NOT NULL
-);
+-- UTILE ???
+-- CREATE TABLE SEANCE (
+--   dateSeance DATE,
+--   seancePayee BOOLEAN,
+--   coursID INT(4) NOT NULL
+-- );
 
 CREATE TABLE TARIF (
   PRIMARY KEY (idT),
-  idT INT(4) NOT NULL,
+  idT INT(4) PRIMARY KEY AUTO_INCREMENT,
   descriptif VARCHAR(42),
   montant DECIMAL(10,2)
 );
@@ -66,16 +64,24 @@ ALTER TABLE RESERVATION ADD FOREIGN KEY (coursID) REFERENCES COURS (coursID);
 ALTER TABLE RESERVATION ADD FOREIGN KEY (poneyID) REFERENCES PONEY (poneyID);
 ALTER TABLE RESERVATION ADD FOREIGN KEY (idM) REFERENCES MEMBRE (idM);
 
-ALTER TABLE SEANCE ADD FOREIGN KEY (coursID) REFERENCES COURS (coursID);
+-- ALTER TABLE SEANCE ADD FOREIGN KEY (coursID) REFERENCES COURS (coursID);
 
 
--- Trigger à revoir
+-- TRIGGER
+
+-- Vérification du poids du cavalier par rapport au poids supportable maximum du poney
 DELIMITER |
-CREATE OR REPLACE TRIGGER poidsInfPoidsMax BEFORE INSERT ON MEMBRE
+CREATE OR REPLACE TRIGGER poidsInfPoidsMax 
+BEFORE INSERT ON RESERVATION
 FOR EACH ROW
 BEGIN
-  IF (SELECT poidsMaxCavalier FROM PONEY WHERE poneyID = :NEW.idM) < :NEW.poidsA THEN
-    RAISE_APPLICATION_ERROR(-20001, 'Le poids du cavalier est supérieur au poids maximum du poney');
+  DECLARE poidsCavalier DECIMAL(10,2);
+  DECLARE poidsSupportableMax DECIMAL(10,2);
+  SELECT poidsA INTO poidsCavalier FROM MEMBRE WHERE idM = :NEW.idM;
+  SELECT poidsSupportableMax INTO poidsSupportableMax FROM PONEY WHERE poneyID = :NEW.poneyID;
+  IF poidsSupportableMax < poidsCavalier THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Le poids du cavalier est supérieur au poids supportable maximum du poney';
   END IF;
 END |
 DELIMITER ;
+
