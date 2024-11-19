306b78962909697abeb61eeff089dbee79270ec9
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2024-11-19 14:39:33 +0100

    Finalisation de la page d'accueil

--- app/app.py
+++ app/app.py
@@ -3,4 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+app.config["SECRET_KEY"] = 'e9fb1c87-3809-4891-88d2-abfcde76d817'
+
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, HiddenField, SubmitField
+from wtforms.validators import DataRequired
 from . import app  # ou import app si app est défini dans __init__.py
 from .models import *
 
@@ -10,9 +13,24 @@ def home():
 def inscription():
     return render_template('inscription.html')
 
+class LoginForm(FlaskForm):
+    email = StringField("Adresse email", validators=[DataRequired()])
+    motdepasse = PasswordField("Mot de passe", validators=[DataRequired()])
+    next = HiddenField()
+    submit = SubmitField("Se connecter")
+
 @app.route('/connexion')
 def connexion():
-    return render_template('connexion.html')
+    form = LoginForm()
+    if not form.is_submitted():
+        form.next.data = request.args.get("next")
+    elif form.validate_on_submit():
+        # if mdp incorrect:
+        #     return render_template('connexion.html', error="Nom d'utilisateur ou mot de passe incorrect", form=form)
+        # next_page = form.next.data or url_for('home')
+        # return redirect(next_page)
+        return redirect(url_for('home'))
+    return render_template('connexion.html', form=form)
 
 @app.route('/calendrier')
 def calendrier():
